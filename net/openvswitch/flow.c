0a975c0ec81de8b3a38770dbe612b3b9cdd52be5
Gavi Teitz <gavi@mellanox.com>
2021-01-31 14:19:44 +0200

    net: openvswitch: Remove check for tc_recirc_sharing_support
    Remove check for OVS support for TC recirc_id sharing, and just assume
    it is supported. This enables older OVS versions to use the recirc_id
    sharing for ST CT without adding a request for this feature, as they
    already support this sharing. Since this kernel is only targeting OVS
    buids which support TC recirc_id sharing, this check is unnecessary.
    
    Change-Id: Iefd1236f11b924e61b170ad28bde7e9ea89da3a9
    Signed-off-by: Gavi Teitz <gavi@nvidia.com>

--- net/openvswitch/flow.c
+++ net/openvswitch/flow.c
@@ -868,12 +868,8 @@ int ovs_flow_key_extract(const struct ip_tunnel_info *tun_info,
 	key->mac_proto = res;
 
 #if IS_ENABLED(CONFIG_NET_TC_SKB_EXT)
-	if (static_branch_unlikely(&tc_recirc_sharing_support)) {
-		tc_ext = skb_ext_find(skb, TC_SKB_EXT);
-		key->recirc_id = tc_ext ? tc_ext->chain : 0;
-	} else {
-		key->recirc_id = 0;
-	}
+	tc_ext = skb_ext_find(skb, TC_SKB_EXT);
+	key->recirc_id = tc_ext ? tc_ext->chain : 0;
 #else
 	key->recirc_id = 0;
 #endif
